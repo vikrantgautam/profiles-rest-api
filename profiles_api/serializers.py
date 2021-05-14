68abd9398f04de6eb87357b997dd438b6503f8ea
vikrantgautam <vikrantgautam947@gmail.com>
2021-05-14 22:37:15 +0530

    Added user profile feed api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 
 from profiles_api import models
 
+
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     """Serializes a user profile object"""
@@ -28,3 +30,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwards = {'user_profile': {'read_only': True}}
